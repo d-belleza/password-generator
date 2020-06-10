54e4c5d47c885ae0cec4a0e315d18af36a10d96c
davidbelleza <davidbelleza@gmail.com>
2020-06-10 12:58:23 -0500

    final script.js

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,60 @@
 // Assignment code here
-
+var lower = 'abcdefghijklmnopqrstuvwxyz';
+var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var number = '0123456789';
+var symbols = '!#$%&()*+,-./:;<=>?@[]^_`{|}~';
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var generatePassword = function(){
+  var password = "";
+  //Ask user for password length
+  var lengthPrompt = window.prompt("Password length? 8-128 characters");
+  lengthPrompt = parseInt(lengthPrompt, 10); //Convert to integer base 10
+
+  if(lengthPrompt >= 8 && lengthPrompt <= 128){
+    //Include character type prompts
+    var lowerPrompt = window.confirm("Include lowercase?");
+    var upperPrompt = window.confirm("Include uppercase?");
+    var numPrompt = window.confirm("Include numbers?");
+    var symPrompt = window.confirm("Include special characters?");
+
+    if(!lowerPrompt && !upperPrompt && !numPrompt && !symPrompt){
+      window.alert("You must have at least one character type."); //Type error handling
+      lowerPrompt = window.confirm("Include lowercase?");
+      upperPrompt = window.confirm("Include uppercase?");
+      numPrompt = window.confirm("Include numbers?");
+      symPrompt = window.confirm("Include special characters?");
+    }
+
+    //String for concatenating char groups 
+    var include = '';
+
+    if(lowerPrompt){
+      include += lower;
+    }
+    if(upperPrompt){
+      include += upper;
+    }
+    if(numPrompt){
+      include += number;
+    }
+    if(symPrompt){
+      include += symbols;
+    }
+
+    //Loop to add characters to password
+    for(i = 0; i < lengthPrompt; i++){
+      password += include[Math.floor(Math.random() * include.length)];
+    }
+  }else{
+    window.alert("Please enter valid length."); //Length error handling
+    generatePassword();
+  }
+  
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
